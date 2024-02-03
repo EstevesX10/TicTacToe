72e68a60b0ba83c0812ec8e6fa535f160c30789a
Gongas <goncalo.mesteves03@gmail.com>
2024-02-03 00:08:26 +0000

    Massive Code Reformulation

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,22 +2,30 @@ import sys
 import pygame
 import random
 import copy
-from time import sleep
 from math import inf
 import numpy as np
+from Button import (Button)
+from Constants import (WIDTH,
+                       HEIGHT,
+                       ROWS,
+                       COLS,
+                       SQSIZE,
+                       LINE_WIDTH,
+                       BG_COLOR,
+                       LINES_COLOR,
+                       BLACK,
+                       WHITE,
+                       CIRC_COLOR,
+                       CIRC_WIDTH,
+                       RADIUS,
+                       X_COLOR,
+                       X_WIDTH,
+                       OFFSET)
 
-from Const import *
-from Button import Button
-
-pygame.init()
-Screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Tic Tac Toe -> AI')
-Icon = pygame.image.load('Extras/Icon.png').convert_alpha()
-pygame.display.set_icon(Icon)
 
 class Board:
-
-    def __init__(self):
+    def __init__(self, screen):
+        self.screen = screen
         self.squares = np.zeros((ROWS,COLS))
         self.empty_squares = self.squares # List of Empty Squares
         self.marked_squares = 0 # Number os Squares Occupied
@@ -37,7 +45,7 @@ class Board:
                     color = CIRC_COLOR if self.squares[0][col] == 2 else X_COLOR
                     Initial_Pos = (col * SQSIZE + SQSIZE // 2, 20)
                     Final_Pos = (col * SQSIZE + SQSIZE // 2, HEIGHT - 20)
-                    pygame.draw.line(Screen, color, Initial_Pos, Final_Pos, LINE_WIDTH)
+                    pygame.draw.line(self.screen, color, Initial_Pos, Final_Pos, LINE_WIDTH)
                 
                 # Returns Player Number
                 return self.squares[0][col]
@@ -50,7 +58,7 @@ class Board:
                     color = CIRC_COLOR if self.squares[row][0] == 2 else X_COLOR
                     Initial_Pos = (20, row * SQSIZE + SQSIZE // 2)
                     Final_Pos = (WIDTH - 20, row * SQSIZE + SQSIZE // 2)
-                    pygame.draw.line(Screen, color, Initial_Pos, Final_Pos, LINE_WIDTH)
+                    pygame.draw.line(self.screen, color, Initial_Pos, Final_Pos, LINE_WIDTH)
                 
                 # Returns Player Number
                 return self.squares[row][0]
@@ -62,7 +70,7 @@ class Board:
                 color = CIRC_COLOR if self.squares[1][1] == 2 else X_COLOR
                 Initial_Pos = (20, 20)
                 Final_Pos = (WIDTH- 20, HEIGHT - 20)
-                pygame.draw.line(Screen, color, Initial_Pos, Final_Pos, X_WIDTH)
+                pygame.draw.line(self.screen, color, Initial_Pos, Final_Pos, X_WIDTH)
             
             return self.squares[1][1]
 
@@ -73,7 +81,7 @@ class Board:
                 color = CIRC_COLOR if self.squares[1][1] == 2 else X_COLOR
                 Initial_Pos = (20, HEIGHT - 20)
                 Final_Pos = (WIDTH - 20, 20)
-                pygame.draw.line(Screen, color, Initial_Pos, Final_Pos, X_WIDTH)
+                pygame.draw.line(self.screen, color, Initial_Pos, Final_Pos, X_WIDTH)
             
             return self.squares[1][1]
 
@@ -102,7 +110,6 @@ class Board:
         return self.marked_squares == 0
 
 class AI:
-
     def __init__(self, level=1, player=2):
         # level = 0 -> Random AI
         # level = 1 -> AI with MiniMax
@@ -179,12 +186,12 @@ class AI:
         return Move
 
 class Game:
-
-    def __init__(self):
-        self.board = Board()
+    def __init__(self, screen):
+        self.board = Board(screen)
         self.ai = AI()
         self.player = 1 # PLayer 1 -> 'X' || Player 2 -> 'O'
-        self.Game_Mode = 'AI' # PVP or AI
+        self.game_mode = 'AI' # PVP or AI
+        self.screen = screen
         self.running = True 
         self.Show_Lines()
     
@@ -195,15 +202,15 @@ class Game:
 
     def Show_Lines(self):
         # Paint Screen
-        Screen.fill(BG_COLOR)
+        self.screen.fill(BG_COLOR)
         
         # Vertical Lines
-        pygame.draw.line(Screen, LINES_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
-        pygame.draw.line(Screen, LINES_COLOR, (WIDTH-SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(self.screen, LINES_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(self.screen, LINES_COLOR, (WIDTH-SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
 
         # Horizontal Lines
-        pygame.draw.line(Screen, LINES_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
-        pygame.draw.line(Screen, LINES_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
+        pygame.draw.line(self.screen, LINES_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
+        pygame.draw.line(self.screen, LINES_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
 
     def Draw_Fig(self, row, col):
         if self.player == 1:
@@ -211,28 +218,23 @@ class Game:
             # Descending line
             start_desc = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
             end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
-            pygame.draw.line(Screen, X_COLOR, start_desc, end_desc, X_WIDTH)
+            pygame.draw.line(self.screen, X_COLOR, start_desc, end_desc, X_WIDTH)
 
             # Ascending line
             start_asc = (col * SQSIZE + OFFSET, row * SQSIZE + SQSIZE - OFFSET)
             end_asc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + OFFSET)
-            pygame.draw.line(Screen, X_COLOR, start_asc, end_asc, X_WIDTH)
+            pygame.draw.line(self.screen, X_COLOR, start_asc, end_asc, X_WIDTH)
 
         elif(self.player == 2):
             # Draw 'O'
             center = (col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2)
-            pygame.draw.circle(Screen, CIRC_COLOR, center, RADIUS, CIRC_WIDTH)
+            pygame.draw.circle(self.screen, CIRC_COLOR, center, RADIUS, CIRC_WIDTH)
 
     def Next_Turn(self):
         self.player = 1 if self.player == 2 else 2
-        # self.player = self.player % 2 + 1
 
     def Change_Game_Mode(self):
-        self.Game_Mode = 'AI' if self.Game_Mode == 'PVP' else 'PVP'
-        # if self.Game_Mode == 'PVP':
-        #     self.Game_Mode = 'AI'
-        # else:
-        #     self.Game_Mode = 'PVP'
+        self.game_mode = 'AI' if self.game_mode == 'PVP' else 'PVP'
 
     def IsOver(self):
         return self.board.Final_State(show=True) != 0 or self.board.Is_Full()
@@ -240,220 +242,169 @@ class Game:
     def Reset(self):
         self.__init__()
 
-def Main():
-    
-    game = Game()
-    board = game.board
-    ai = game.ai
-    
-    # MAIN LOOP
-    while True:
+    def run(self, AI_Level, Mode):
+        self.ai.level = AI_Level
+        self.game_mode = Mode
 
-        for event in pygame.event.get():
-            
-            if event.type == pygame.KEYDOWN:
-                # Press g -> Change Game Mode
-                if event.key == pygame.K_g:
-                    game.Change_Game_Mode()
+        # MAIN LOOP
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    # Press g -> Change Game Mode
+                    if event.key == pygame.K_g:
+                        self.Change_Game_Mode()
 
-                if event.key == pygame.K_r:
-                    game.Reset()
-                    board = game.board
-                    ai = game.ai
+                    if event.key == pygame.K_r:
+                        self.Reset()
+                        board = self.board
+                        self.game_mode = Mode
+                        self.ai.level = AI_Level 
+                        ai = self.ai
 
-                # 0 -> Random AI
-                if event.key == pygame.K_0:
-                    ai.level = 0
-
-                # 1 -> Expert AI
-                if event.key == pygame.K_1:
-                    ai.level = 1
-            
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = event.pos
-                row = pos[1] // SQSIZE
-                col = pos[0] // SQSIZE
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
                 
-                if board.Empty_Square(row, col) and game.running:
-                    game.Make_Move(row, col)
-                    if game.IsOver():
-                        game.running = False
-        
-        if game.Game_Mode == "AI" and game.player == ai.player and game.running:
-            # Update the Screen
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = event.pos
+                    row = pos[1] // SQSIZE
+                    col = pos[0] // SQSIZE
+                    
+                    if board.Empty_Square(row, col) and self.running:
+                        self.Make_Move(row, col)
+                        if self.IsOver():
+                            self.running = False
+            
+            if self.game_mode == "AI" and self.player == ai.player and self.running:
+                # Update the Screen
+                pygame.display.update()
+                
+                # AI Methods
+                row, col = ai.Evaluate(board)
+                self.Make_Move(row, col)
+
+                if self.IsOver():
+                    self.running = False
+
             pygame.display.update()
-            
-            # AI Methods
-            row, col = ai.Evaluate(board)
-            game.Make_Move(row, col)
 
-            if game.IsOver():
-                        game.running = False
+class TIC_TAC_TOE():
+    def __init__(self):
+        # Initializing
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Tic Tac Toe [AI]')
+        Icon = pygame.image.load('Extras/Icon.png').convert_alpha()
+        pygame.display.set_icon(Icon)
 
-        pygame.display.update()
+        # Board & Game Mode Variables
+        self.game = Game(self.screen)
+        self.board = self.game.board
+        self.ai = self.game.ai
 
-def Main_V2(AI_Level, Mode):
+        # Fonts
+        self.My_Font = pygame.font.SysFont("arialblack", 22)
+        self.My_Big_Font = pygame.font.SysFont("arialblack", 30)
+
+        # Main Application Variables
+        self.Run = True
+        self.Menu = "Main"
+
+    def Write(self, text, my_font, color, x, y, screen):
+        img = my_font.render(text, True, color)
+        screen.blit(img, (x,y))
     
-    game = Game()
-    board = game.board
-    game.Game_Mode = Mode
-    game.ai.level = AI_Level 
-    ai = game.ai
-    
-    # MAIN LOOP
-    while True:
+    def run(self):
 
-        for event in pygame.event.get():
-            
-            if event.type == pygame.KEYDOWN:
-                # Press g -> Change Game Mode
-                if event.key == pygame.K_g:
-                    game.Change_Game_Mode()
+        # Images & Buttons
+        Main_Menu_BG_IMG = pygame.image.load('Extras/BG_IMG.jpg').convert_alpha()
+        Main_Menu_BG = Button(650, -30, Main_Menu_BG_IMG, .6)
 
-                if event.key == pygame.K_r:
-                    game.Reset()
-                    board = game.board
-                    game.Game_Mode = Mode
-                    game.ai.level = AI_Level 
-                    ai = game.ai
+        Game_Mode_BG_IMG = pygame.image.load('Extras/BG_IMG_2.jpg').convert_alpha()
+        Game_Mode_BG = Button(700, 0, Game_Mode_BG_IMG, .7)
 
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = event.pos
-                row = pos[1] // SQSIZE
-                col = pos[0] // SQSIZE
+        Play_IMG = pygame.image.load('Extras/Start_3.png').convert_alpha()
+        Play_Button = Button(220,200,Play_IMG, .3)
+
+        Back_IMG = pygame.image.load('Extras/Back.png').convert_alpha()
+        Back_Button = Button(580,20,Back_IMG, .15)
+
+        PVP_IMG = pygame.image.load('Extras/PVP.png').convert_alpha()
+        PVP_Button = Button(250, 130, PVP_IMG, .2)
+
+        AI_IMG = pygame.image.load('Extras/AI.png').convert_alpha()
+        AI_Button = Button(450, 130, AI_IMG, .2)
+
+        AI_BG_IMG = pygame.image.load('Extras/BG_IMG_3.jpg').convert_alpha()
+        AI_BG = Button(920,0,AI_BG_IMG, .6)
+
+        AI_RANDOM_IMG = pygame.image.load('Extras/Random.png').convert_alpha()
+        AI_RANDOM = Button(150, 180, AI_RANDOM_IMG, .2)
+
+        AI_MINIMAX_IMG = pygame.image.load('Extras/MiniMax.png').convert_alpha()
+        AI_MINIMAX = Button(575, 180, AI_MINIMAX_IMG, .2)
+
+        EXIT_IMG = pygame.image.load('Extras/Exit.png').convert_alpha()
+        EXIT_Button = Button(575, 500, EXIT_IMG, .2)
+
+        while self.Run:
+
+            if self.Menu == "Main":
+                Main_Menu_BG.Show(self.screen)
+                if Play_Button.Action(self.screen):
+                    self.Menu = "Game_Mode"
+
+            elif self.Menu == "Game_Mode":
+                Game_Mode_BG.Show(self.screen)
+                self.Write("GAME MODE", self.My_Big_Font, BLACK, 200, 50, self.screen)
+                self.Write("PVP Mode", self.My_Font, BLACK, 140, 250, self.screen)
+                self.Write("AI Mode", self.My_Font, BLACK, 350, 250, self.screen)
                 
-                if board.Empty_Square(row, col) and game.running:
-                    game.Make_Move(row, col)
-                    if game.IsOver():
-                        game.running = False
-        
-        if game.Game_Mode == "AI" and game.player == ai.player and game.running:
-            # Update the Screen
+                if AI_Button.Action(self.screen):
+                    # Activate AI Mode
+                    self.Menu = "AI"
+                    
+                if PVP_Button.Action(self.screen):
+                    # Activate PVP Mode
+                    print("PVP MODE")
+                    self.game.run(1, "PVP")
+
+                if Back_Button.Action(self.screen):
+                    self.Menu = "Main"
+                
+                if EXIT_Button.Action(self.screen):
+                    self.Run = False
+
+            elif self.Menu == "AI":
+                AI_BG.Show(self.screen)
+                self.Write("AI MODE", self.My_Big_Font, WHITE, 230, 50, self.screen)
+                self.Write("Random", self.My_Font, WHITE, 48, 300, self.screen)
+                self.Write("Choice", self.My_Font, WHITE, 56, 330, self.screen)
+                self.Write("MiniMax", self.My_Font, WHITE, 475, 300, self.screen)
+
+                if AI_RANDOM.Action(self.screen):
+                    print("AI RANDOM")
+                    self.game.run(0, "AI")
+
+                if AI_MINIMAX.Action(self.screen):
+                    print("AI MINIMAX")
+                    self.game.run(1, "AI")
+
+                if Back_Button.Action(self.screen):
+                    self.Menu = "Game_Mode"
+                
+                if EXIT_Button.Action(self.screen):
+                    self.Run = False
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    pass
+
+                if event.type == pygame.QUIT:
+                    self.Run = False
+            
             pygame.display.update()
-            
-            # AI Methods
-            row, col = ai.Evaluate(board)
-            game.Make_Move(row, col)
+        pygame.quit()
 
-            if game.IsOver():
-                game.running = False
-
-        pygame.display.update()
-
-# Colors
-Black = (0,0,0)
-White = (255,255,255)
-
-# Letras
-Tipo_Letra = pygame.font.SysFont("arialblack", 22)
-Tipo_Letra_Grande = pygame.font.SysFont("arialblack", 30)
-
-def Escrever(texto, tipo_letra, cor, x, y, tela):
-    img = tipo_letra.render(texto, True, cor)
-    tela.blit(img, (x,y))
-
-def Start_Game():
-    Run = True
-    Menu = "Main"
-    
-    # Variables
-    Main_Menu_BG_IMG = pygame.image.load('Extras/BG_IMG.jpg').convert_alpha()
-    Main_Menu_BG = Button(650, -30, Main_Menu_BG_IMG, .6)
-
-    Game_Mode_BG_IMG = pygame.image.load('Extras/BG_IMG_2.jpg').convert_alpha()
-    Game_Mode_BG = Button(700, 0, Game_Mode_BG_IMG, .7)
-
-    Play_IMG = pygame.image.load('Extras/Start_3.png').convert_alpha()
-    Play_Button = Button(220,200,Play_IMG, .3)
-
-    Back_IMG = pygame.image.load('Extras/Back.png').convert_alpha()
-    Back_Button = Button(580,20,Back_IMG, .15)
-
-    PVP_IMG = pygame.image.load('Extras/PVP.png').convert_alpha()
-    PVP_Button = Button(250, 130, PVP_IMG, .2)
-
-    AI_IMG = pygame.image.load('Extras/AI.png').convert_alpha()
-    AI_Button = Button(450, 130, AI_IMG, .2)
-
-    AI_BG_IMG = pygame.image.load('Extras/BG_IMG_3.jpg').convert_alpha()
-    AI_BG = Button(920,0,AI_BG_IMG, .6)
-
-    AI_RANDOM_IMG = pygame.image.load('Extras/Random.png').convert_alpha()
-    AI_RANDOM = Button(150, 180, AI_RANDOM_IMG, .2)
-
-    AI_MINIMAX_IMG = pygame.image.load('Extras/MiniMax.png').convert_alpha()
-    AI_MINIMAX = Button(575, 180, AI_MINIMAX_IMG, .2)
-
-    EXIT_IMG = pygame.image.load('Extras/Exit.png').convert_alpha()
-    EXIT_Button = Button(575, 500, EXIT_IMG, .2)
-    
-    while Run:
-
-        if Menu == "Main":
-            Main_Menu_BG.Show(Screen)
-            if Play_Button.Action(Screen):
-                Menu = "Game_Mode"
-
-        elif Menu == "Game_Mode":
-            Game_Mode_BG.Show(Screen)
-            Escrever("GAME MODE", Tipo_Letra_Grande, Black, 200, 50, Screen)
-            Escrever("PVP Mode", Tipo_Letra, Black, 140, 250, Screen)
-            Escrever("AI Mode", Tipo_Letra, Black, 350, 250, Screen)
-            
-            if AI_Button.Action(Screen):
-                # Activate AI Mode
-                Menu = "AI"
-                
-            if PVP_Button.Action(Screen):
-                # Activate PVP Mode
-                print("PVP MODE")
-                Main_V2(1, "PVP")
-
-            if Back_Button.Action(Screen):
-                Menu = "Main"
-            
-            if EXIT_Button.Action(Screen):
-                Run = False
-
-        elif Menu == "AI":
-            AI_BG.Show(Screen)
-            Escrever("AI MODE", Tipo_Letra_Grande, White, 230, 50, Screen)
-            Escrever("Random", Tipo_Letra, White, 48, 300, Screen)
-            Escrever("Choice", Tipo_Letra, White, 56, 330, Screen)
-            Escrever("MiniMax", Tipo_Letra, White, 475, 300, Screen)
-
-            if AI_RANDOM.Action(Screen):
-                print("AI RANDOM")
-                Main_V2(0, "AI")
-
-            if AI_MINIMAX.Action(Screen):
-                print("AI MINIMAX")
-                Main_V2(1, "AI")
-
-            if Back_Button.Action(Screen):
-                Menu = "Game_Mode"
-            
-            if EXIT_Button.Action(Screen):
-                Run = False
-
-        for event in pygame.event.get():
-            
-            if event.type == pygame.KEYDOWN:
-                pass
-
-            if event.type == pygame.QUIT:
-                Run = False
-        
-        pygame.display.update()
-    pygame.quit()
-
-# Main()
-Start_Game()
+Tic_Tac_Toe = TIC_TAC_TOE()
+Tic_Tac_Toe.run()
